ccba37f076694e4b997e946ef72cb41fc9318f63
mohdjahid <mohd.jahid.jmi@gmail.com>
2015-12-01 15:29:48 +0530

    Collection Framework
    How to use each class and interface of collection framework

---
+++ Collection/StackDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class StackDemo
+{
+	public static void main(String... args)
+	{
+		Stack s=new Stack();
+		s.push("A");
+		s.push("B");
+		s.push("C");
+		s.pop();
+		System.out.println(s);
+		System.out.println(s.search("A"));
+		System.out.println(s.search("Z"));
+	}
+}
